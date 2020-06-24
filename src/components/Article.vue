4da83725f989cbdb3a047319457e5a2c7d983309
twtrubiks <jj860528@gmail.com>
2020-06-24 23:43:15 +0800

    loading調整

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="article" :id="article.id">
-    <div class="article-img img" :style="img"></div>
+    <div class="article-img img" :style="img" @click="go()"></div>
     <div class = "article-container">
-      <h2 class="article-title">{{article.name}}</h2>
+      <h2 class="article-title" @click="go()">{{article.name}}</h2>
       <pre class="article-content" v-html="content[0].content"></pre>
     </div>
   </div>
@@ -19,13 +19,18 @@ export default {
   data() {
     return {
       article: this.Article,
-      content: this.Article.content || 1231,
+      content: this.Article.content || '暫無內容',
       img: {
         backgroundImage: "url(" + this.Article.img + ")",
         backgroundPosition: "center center",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover"
       }
+    }
+  },
+  methods:{
+    go:()=>{
+      alert('暫無此頁')
     }
   },
   watch:{
