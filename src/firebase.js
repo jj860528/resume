02497fd4766be8cbface2b06ea778f8bd90a6bbd
twtrubiks <jj860528@gmail.com>
2020-07-01 17:43:51 +0800

    專案移動到firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,47 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAVpJe2882o24RSqPZiulPRXxwKiDTss-c",
-    authDomain: "blog-b8466.firebaseapp.com",
-    databaseURL: "https://blog-b8466.firebaseio.com",
-    projectId: "blog-b8466",
-    storageBucket: "blog-b8466.appspot.com",
-    messagingSenderId: "519041697145",
-    appId: "1:519041697145:web:4fcbb6e87ff9695aef1ba8",
-    measurementId: "G-5NB5LGHTP6"
-  };
+  apiKey: "AIzaSyAVpJe2882o24RSqPZiulPRXxwKiDTss-c",
+  authDomain: "blog-b8466.firebaseapp.com",
+  databaseURL: "https://blog-b8466.firebaseio.com",
+  projectId: "blog-b8466",
+  storageBucket: "blog-b8466.appspot.com",
+  messagingSenderId: "519041697145",
+  appId: "1:519041697145:web:4fcbb6e87ff9695aef1ba8",
+  measurementId: "G-5NB5LGHTP6"
+};
 
-import firebase from "firebase";
-import "firebase/firestore";
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+//初始化
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
-const database = firebase.database();
-export default { db, database };
+
+//獲取初始資料
+const projectRef = db.collection("projects")
+const project = []
+//所有子集合
+//projects
+projectRef.get().then((data) => {
+  data.forEach((doc) => {
+    project.push(doc.data())
+  })
+})
+
+//articles
+const articlesRef= db.collection("articles")
+const articles = []
+articlesRef.get().then((data) => {
+  data.forEach((doc) => {
+    articles.push(doc.data())
+  })
+})
+
+/*新增資料
+const citiesRef = db.collection('//文件名//');
+
+let setSf = citiesRef.doc("//放入文件名+id//").set({
+  //放入data
+});*/
+
+//匯出至store
+export default { project , articles};
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import FrebaseData from "../firebase.js";
 
 Vue.use(Vuex);
 
@@ -33,84 +34,8 @@ export default new Vuex.Store({
         ]
       }
     ],
-    projects:[
-      {
-        ID:"01",
-        Date:"2020 05",
-        name:"仿訂票網站",
-        introduction: "選座系統開發",
-        url:"https://jj860528.github.io/movie/",
-        img:"https://i.imgur.com/jxpDywk.png",
-        skills:[
-          {skill:"Element-Vue"},
-          {skill:"Vue"},
-          {skill:"Veux"},
-          {skill:"Vue-Router"},
-        ],
-      },
-      {
-        ID:"02",
-        Date:"2020 06",
-        name:"Web切版",
-        introduction: "純css切版",
-        url:"https://ucjjje6de17g51zkpwk3uq-on.drv.tw/web/webRWD/webDesign1.html#header",
-        img:"https://i.imgur.com/jAmpNkm.png",
-        skills:[
-          {skill:"CSS3"},
-          {skill:"RED切版"},
-          {skill:"fontawesome-Icon"},
-          {skill:"imgur線上圖片"},
-        ],
-      }
-    ],
-    articles:[
-      {
-        id: 'Articles01',
-        name:'Google第三方登入實作',
-        date:'2020 06',
-        img:'https://i.imgur.com/5Fa5Myg.jpg',//略縮圖
-        content:[
-          {
-            order:'01',
-            name:'text',
-            content:'<p>現在其實可以發現<br />許多網站和線上遊戲<br />都有支援第三方登入<br />方便使用者不用記帳號(email)和密碼<br />GCP OAuth算是普遍大家都有的服務<br />所以開發使用者登入功能時<br />別忘了第三方登入這一塊</p>'
-          },
-          {
-            order:"02",
-            name:"pre",
-            content:'<p><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;p&gt;內容&lt;/ p&gt; </span></span><br /><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;p&gt;＆nbsp; &lt;/ p&gt; </span></span><br /><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;p&gt; function（）{&lt;/ p&gt; </span></span><br /><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;p&gt;＆nbsp; </span><span style="vertical-align: inherit;">＆nbsp; 121 561 65 1 &lt;/ p&gt; </span></span><br /><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;p&gt;} &lt;/ p&gt;</span></span></p>'
-          },
-          {
-            order:"03",
-            name:"img",
-            content:"圖片地址"
-          }
-        ]
-      },
-      /*{
-        id: 'Articles02',
-        name:'Google第三方登入',
-        date:'2020 06',
-        img:'https://i.imgur.com/5Fa5Myg.jpg',//略縮圖
-        content:[
-          {
-            order:'01',
-            name:'text',
-            content:'<p><em><strong>以下為例子~~</strong></em></p>'
-          },
-          {
-            order:"02",
-            name:"pre",
-            content:'<p><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;p&gt;內容&lt;/ p&gt; </span></span><br /><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;p&gt;＆nbsp; &lt;/ p&gt; </span></span><br /><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;p&gt; function（）{&lt;/ p&gt; </span></span><br /><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;p&gt;＆nbsp; </span><span style="vertical-align: inherit;">＆nbsp; 121 561 65 1 &lt;/ p&gt; </span></span><br /><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;p&gt;} &lt;/ p&gt;</span></span></p>'
-          },
-          {
-            order:"03",
-            name:"img",
-            content:"圖片地址"
-          }
-        ]
-      }*/
-    ]
+    projects:FrebaseData.project,
+    articles:FrebaseData.articles
   },
   mutations: {},
   actions: {},
